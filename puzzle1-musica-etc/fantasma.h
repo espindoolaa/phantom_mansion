5eccf46744b36777612be2d3a2596456e344fbee
Mateus Espíndola <101195071+espindoolaa@users.noreply.github.com>
2025-04-06 23:40:32 -0300

    feat/new sprites for kitchen

---
+++ puzzle1-musica-etc/fantasma.h
@@ -0,0 +1,13 @@
+#ifndef FANTASMA_H
+#define FANTASMA_H
+#include "raylib.h"
+
+typedef struct Fantasma{
+    float position_x;
+    float position_y;
+    Texture desenho;
+    Rectangle hitbox;
+}Fantasma;
+
+#include "fantasma.c"
+#endif
