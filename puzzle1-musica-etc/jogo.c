006724c54a1bfc5571f2ceecc4f7fbe0752a1707
Mateus Espíndola <101195071+espindoolaa@users.noreply.github.com>
2025-04-08 17:18:27 -0300

    Add files via upload

--- puzzle1-musica-etc/jogo.c
+++ puzzle1-musica-etc/jogo.c
@@ -7,8 +7,7 @@
 
 
 int main(){
-    const int Largura = 1366, Altura = 768;
-    //const int Largura = GetMonitorWidth(0), Altura = GetMonitorHeight(0);
+    const int Largura = 1920, Altura = 1080;
 
     InitWindow(Largura, Altura, "Phantom Mansion");
     InitAudioDevice(); // pode ter audio agora 
@@ -16,12 +15,14 @@ int main(){
     bool PodeAndar = true;
     bool dica_na_tela = false;
     bool cofre_na_tela = false;
+    bool geladeira_na_tela = false; 
+
     int senha = 0;
     char cod[4];
 
     //area dos sprites
-    Texture fundo_mapa1, sprite_personagem, sprite_baronesa, sprite_diario, sprite_lamp, sprite_lamp_fire, sprite_oil, sprite_dica_p1, sprite_geladeira;
-    fundo_mapa1 = LoadTexture("./backgrounds/background1f.jpeg");
+    Texture fundo_mapa, sprite_personagem, sprite_alicate, sprite_baronesa, sprite_diario, sprite_lamp, sprite_lamp_fire, sprite_oil, sprite_dica_p1, sprite_geladeira;
+    fundo_mapa = LoadTexture("./backgrounds/cozinha.png");
 
     sprite_personagem = LoadTexture("./sprites/spritesheet_personagem.png");
     sprite_baronesa = LoadTexture("./sprites/spritesheet_fantasma.png");
@@ -30,13 +31,16 @@ int main(){
     sprite_lamp = LoadTexture("./sprites/lamp.png");
     sprite_lamp_fire = LoadTexture("./sprites/light lamp.png");//
     sprite_oil = LoadTexture("./sprites/oil.png");
-    sprite_dica_p1 = LoadTexture("./sprites/dica_1_atualizado.png");
-    
+    sprite_dica_p1 = LoadTexture("./sprites/dicap1.png");
+
+    sprite_alicate = LoadTexture("./sprites/alicate.png");
     sprite_geladeira = LoadTexture("./sprites/fridge_puzzle.png");
+    
 
     //personagem
     Personagem info_personagem;
     info_personagem = CreatePerson(Largura/2 - 30, 900, sprite_personagem);
+    
     Rectangle frame_personagem = (Rectangle){0, 63,39 ,84};
 
     //fantasma
@@ -48,12 +52,20 @@ int main(){
     Item info_dica_p1 = CreateItem(1500, 900, sprite_diario, 0);
     //Item info_diario1 = CreateItem(1500, 900, sprite_diario);
 
-    Item info_lamp = CreateItem(1160 * scale_up, 20 * scale_up, sprite_lamp, 0);
+    Item info_lamp = CreateItem(1160 , 20 * scale_up, sprite_lamp, 0);
     info_lamp.coletavel = true;
     Item info_oil = CreateItem(25 * scale_up, 800 * scale_up, sprite_oil, 1);
     info_oil.coletavel = true;
 
+    // Alicate 
+    Item info_alicate = CreateItem(1500, 193, sprite_alicate, 1);
+    info_alicate.coletavel = true;
+    
+
     Item info_cofre = CreateItem(145 * scale_up, 150 * scale_up, sprite_diario, 0);
+
+    // Geladeira 
+    Item info_geladeira = CreateItem(810, 200, sprite_diario, 0);
 
     //musicas e sons
     Music musica_fase1 = LoadMusicStream("./sound/firstfloor.mp3");
@@ -72,12 +84,11 @@ int main(){
     bool fechar_jogo = false;
     SetTargetFPS(120);
     SetWindowSize(Largura, Altura);
-
+    ToggleFullscreen();
     while (!fechar_jogo){
         fechar_jogo = (WindowShouldClose());
         HideCursor();
 
-        //menu
 
         if(IsKeyPressed(KEY_SPACE)){//mudar quando entrar na fase 2
             musica = musica_fase2;
@@ -95,28 +106,21 @@ int main(){
 
         BeginDrawing();
         ClearBackground(WHITE);
-
-        DrawTextureEx(fundo_mapa1, (Vector2){0,0}, 0, scale_up, WHITE);
-
-        //retangulo preto no escritorio
-        DrawTextureEx(sprite_diario, (Vector2){info_dica_p1.position_x, info_dica_p1.position_y}, 0, 4, WHITE);
-        if(LampCheck(&info_personagem) == 0){
-            DrawRectangle(980 * scale_up, 460 * scale_up, 945 * scale_up, 445 * scale_up, BLACK);
+        
+        DrawTextureEx(fundo_mapa, (Vector2){0,0}, 0, 1, WHITE);
+        
+        //Interação com a geladeira 
+        if(InteracPerson(&info_personagem, &info_geladeira) == 1){
+            geladeira_na_tela = !geladeira_na_tela; 
+            PodeAndar = !geladeira_na_tela; 
         }
-
-        //interacao com a dica da fase 1
-        if(InteracPerson(&info_personagem, &info_dica_p1) == 1){
-            dica_na_tela = !dica_na_tela; 
-            PodeAndar = !dica_na_tela; 
-        }
-        if(dica_na_tela){
-            DrawTextureEx(sprite_dica_p1,(Vector2){640, 160}, 0, 1, WHITE);//
-        }
+    
 
         //efeito sonoro de coletar
         if(InteracPerson(&info_personagem, &info_oil) == 2||InteracPerson(&info_personagem, &info_lamp) == 2){
             PlaySound(collect); 
         }
+        
 
         //interacao com o cofre
         if(InteracPerson(&info_personagem, &info_cofre) == 1){
@@ -127,19 +131,27 @@ int main(){
                 cod[senha] = '\0';
             }
         }
+
         CofreCheck(cofre_na_tela, cod, &senha, &info_personagem);
     
-        //DrawRectangleRec(info_personagem.hitbox, BLUE);
+        DrawRectangleRec(info_personagem.hitbox, BLUE);
         DrawTexturePro(sprite_personagem, frame_personagem, info_personagem.hitbox, (Vector2){info_personagem.hitbox.width - 13*size, info_personagem.hitbox.height - 21*size}, 0, WHITE);
+        
+        // Desenho da geladeira acima do personagem principal 
+        if(geladeira_na_tela){
+            DrawTextureEx(sprite_geladeira,(Vector2){400, 40}, 0, 1, WHITE);//
+        }
+        
         //DrawRectangleRec(info_baronesa.hitbox, RED);
-        DrawTexturePro(sprite_baronesa, frame_baronesa, info_baronesa.hitbox, (Vector2){info_baronesa.hitbox.width - 15*size, info_baronesa.hitbox.height - 20*size}, 0, WHITE);
+        DrawTexturePro(sprite_baronesa, frame_baronesa, info_baronesa.hitbox, (Vector2){info_baronesa.hitbox.width - 15*size, info_baronesa.hitbox.height - 20*size}, 0,WHITE);
         DrawTextureEx(sprite_lamp, (Vector2){info_lamp.position_x, info_lamp.position_y}, 0, 5, WHITE);//
+        DrawTextureEx(sprite_alicate, (Vector2){info_alicate.position_x, info_alicate.position_y}, 0, 1.8, WHITE);//
         DrawTextureEx(sprite_oil, (Vector2){info_oil.position_x, info_oil.position_y}, 0, 5, WHITE); //
         
         EndDrawing();
     }
 
-    UnloadTexture(fundo_mapa1);
+    UnloadTexture(fundo_mapa);
 
     UnloadTexture(sprite_personagem);
     UnloadTexture(sprite_baronesa);
@@ -148,8 +160,7 @@ int main(){
     UnloadTexture(sprite_lamp);
     UnloadTexture(sprite_lamp_fire);
     UnloadTexture(sprite_oil);
-
-    UnloadTexture(sprite_geladeira);
+    UnloadTexture(sprite_alicate);
 
     UnloadMusicStream(musica_fase1);     
     UnloadMusicStream(musica_fase2);   
